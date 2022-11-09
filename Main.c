319505035b33a02109e2a996c8807664e5488c07
JunBumHan <2hanjunbum6@gmail.com>
2022-11-09 21:06:42 +0900

    [구현] title

---
+++ Main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "sort.h"
+
+void title(void) {
+    puts("\n");
+    puts("                        m    m    m   \"");
+    puts("  mmm    mmm    m mm  mm#mm  \"m  m\" mmm     mmm  m     m");
+    puts(" #   \"  #\" \"#   #\"  \"   #     #  #    #    #\"  # \"m m m\"");
+    puts("  \"\"\"m  #   #   #       #     \"mm\"    #    #\"\"\"\"  #m#m#");
+    puts(" \"mmm\"  \"#m#\"   #       \"mm    ##   mm#mm  \"#mm\"   # #");
+    puts("\n");
+}
+
+int main() {
+    title();
+
+    
+}
