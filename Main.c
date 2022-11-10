a68920b02e9efc8eb666ddf854353dc26ccc5997
JunBumHan <2hanjunbum6@gmail.com>
2022-11-10 22:20:54 +0900

    [구현] Sort fix

--- Main.c
+++ Main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include <time.h>
 #include "sort.h"
 
-void title(void) {
-    puts("\n");
-    puts("                        m    m    m   \"");
-    puts("  mmm    mmm    m mm  mm#mm  \"m  m\" mmm     mmm  m     m");
-    puts(" #   \"  #\" \"#   #\"  \"   #     #  #    #    #\"  # \"m m m\"");
-    puts("  \"\"\"m  #   #   #       #     \"mm\"    #    #\"\"\"\"  #m#m#");
-    puts(" \"mmm\"  \"#m#\"   #       \"mm    ##   mm#mm  \"#mm\"   # #");
-    puts("\n");
-}
-
 int main() {
-    title();
-
     
+    struct Sort * sort = sort_init();
+    
+    printf("정렬 전: ");
+    srand((unsigned)time(NULL));
+    for(int i = 0; i < 25; ++i) {
+        printf("%d ", sort->arr[i] = rand()%101);
+    }
+
+    printf("sort->arr(%%p) : %p", sort->arr);
+
+    puts("\n");
+    sort->bubleSort(sort);
+
+    // for(int i = 0; i < 25; ++i) {
+    //     printf("%d ", sort->arr[i]);
+    // }
 }
